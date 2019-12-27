1df68a773c6fd4af7e7b04b0b2c95b4dff18b100
Mohammed Nouri <simoiroun25@gmail.com>
2019-12-27 15:10:45 +0100

    project2 done

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 
 function ageInDays() {
 
-  const birthYear = prompt('What year were you born .... good friend?');
+  const birthYear = prompt('What year were you born My friend?');
   const myageInDays = (2019-birthYear) * 365;
   const h1 = document.createElement('h1');
-  const textAnswer = document.createTextNode('You are ' + myageInDays + ' days old')
+  const textAnswer = document.createTextNode('Kevin is  ' + myageInDays + ' days old')
   h1.setAttribute('id', 'ageInDays');
   h1.appendChild(textAnswer);
   document.getElementById('flex-box-result').appendChild(h1);
@@ -16,3 +16,12 @@ function reset() {
   document.getElementById('ageInDays').remove();
 }
 
+//............................................................................
+
+function generateLion() {
+
+  const image = document.createElement('img');
+  let div = document.getElementById('flex-lion-gen');
+  image.src = "https://i.gifer.com/160g.gif"
+  div.appendChild(image);
+}
